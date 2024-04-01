6fee67b9e74b77fcb580827360d7951d7f4d0e69
FirdausyTama <atama.firdausy@gmail.com>
2024-04-01 15:40:45 +0700

    selesai

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+function show() {
+    var confirmation = confirm("Anda akan diarahkan ke halaman lain. Lanjutkan?");
+    if (confirmation) {
+        window.location.href = "#";
+    }
+}
+
+/* gabisa nyambung ke js padahal udh id sambung knp ya*/
